93d4f511c207b34ff89b63875205dfe844e009cc
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2025-12-01 10:41:41 +0100

    refactor: rename to expr to ast for consistency

--- src/passes.py
+++ src/passes.py
@@ -18,16 +18,16 @@ def beta_reduction(ast):
         return [beta_reduction(x) for x in ast]
     return ast
 
-def flatten(expr):
+def flatten(ast):
     """
     Flatten nested `let` expressions into a linear sequence of bindings.
     Input is assumed to already be in ANF form.
     Output: (bindings, value)
     """
-    if not isinstance(expr, list):
-        return [], expr
-    elif expr[0] == 'let':
-        bindings, body = expr[1], expr[2]
+    if not isinstance(ast, list):
+        return [], ast
+    elif ast[0] == 'let':
+        bindings, body = ast[1], ast[2]
         acc = []
         for var, val in bindings:
             bnds_v, val_v = flatten(val)
@@ -36,15 +36,15 @@ def flatten(expr):
         bnds_body, body_val = flatten(body)
         acc.extend(bnds_body)
         return acc, body_val
-    elif isinstance(expr, list):
-        subexprs = expr[1:]
+    elif isinstance(ast, list):
+        subexprs = ast[1:]
         acc = []
         flat_subs = []
         for e in subexprs:
             bnds_e, val_e = flatten(e)
             acc.extend(bnds_e)
             flat_subs.append(val_e)
-        return acc, [expr[0]] + flat_subs
+        return acc, [ast[0]] + flat_subs
 
 def flatten_program(expr):
     bindings, value = flatten(expr)
